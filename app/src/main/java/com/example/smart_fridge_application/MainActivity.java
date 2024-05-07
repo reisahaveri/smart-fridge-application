fd418e54ad876570b587d21720383a6fb5783134
reisahaveri <reisa.haveri.001@student.uni.lu>
2024-05-07 02:51:35 +0200

    adding button for scanner

--- app/src/main/java/com/example/smart_fridge_application/MainActivity.java
+++ app/src/main/java/com/example/smart_fridge_application/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.smart_fridge_application;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -11,10 +16,15 @@ import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.example.smart_fridge_application.databinding.ActivityMainBinding;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+
+    Button scan_btn;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +42,34 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+
+        scan_btn = findViewById(R.id.scanner);
+        textView = findViewById(R.id.text);
+        scan_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                IntentIntegrator intentIntegrator = new IntentIntegrator(MainActivity.this);
+                intentIntegrator.setOrientationLocked(true);
+                intentIntegrator.setPrompt("Scan a barcode");
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);//Qr code???
+                intentIntegrator.initiateScan();
+            }
+        });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        IntentResult intentResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if(intentResult != null){
+            String contents = intentResult.getContents();
+            if (contents != null){
+                textView.setText(intentResult.getContents());
+            }
+        }else {
+            super.onActivityResult(requestCode, resultCode, data);
+
+        }
+
+    }
 }
