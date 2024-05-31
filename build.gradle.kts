e79952dd1a25d531aad18010026a9fbac6cc2eec
raissa <amelinenar@gmail.com>
2024-05-31 14:27:50 +0200

    added libraries and functionalities for firebase

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,12 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+//plugins {
+//    alias(libs.plugins.android.application) apply false
+//    alias(libs.plugins.google.gms.google.services) apply false
+//}
 plugins {
-    alias(libs.plugins.android.application) apply false
+    id("com.android.application") version "8.4.0" apply false
+    // ...
+
+    // Add the dependency for the Google services Gradle plugin
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
